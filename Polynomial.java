faa9c87976e7e9b4dda5a179af8ad930586848d2
张浩芊 <zhanghaoqian@ZHQ-MBP-14-D.ht.home>
2022-09-23 21:36:31 -0400

    added Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,44 +1,43 @@
 public class Polynomial{
     double[] coefficients;
 
-    public Polynomial(){
+    public Polynomial() {
         this.coefficients = new double[0];
     }
 
-    public Polynomial(double[] coefficients){
+    public Polynomial(double[] coefficients) {
         this.coefficients = coefficients;
     }
 
-    public Polynomial add(Polynomial other){
+    public Polynomial add(Polynomial other) {
+        double [] array;
+
         int len_this = this.coefficients.length;
         int len_other = other.coefficients.length;
-        int max_len = (len_this > len_other) ? len_this : len_other;
 
-        double[] array = new double[max_len];
+        array = new double[len_this > len_other ? len_this : len_other];
 
-        for(int i = 0; i < this.coefficients.length; i++)
-        {
+        for(int i = 0; i < this.coefficients.length; i++) {
             array[i] = array[i] + this.coefficients[i];
         }
 
-        for(int j = 0; j < other.coefficients.length; j++)
-        {
+        for(int j = 0; j < other.coefficients.length; j++) {
             array[j] = array[j] + other.coefficients[j];
         }
 
         return new Polynomial(array);
     }
 
-    public double evaluate(double x){
-        double sum = 0;
-        for(int i = 0; i < coefficients.length; i++){
+    public double evaluate(double x) {
+        double sum = 0.0;
+        for(int i = 0; i < coefficients.length; i++) {
             double j = coefficients[i];
             sum = sum + j * Math.pow(x, i);
         }
         return sum;
     }
 
-    public boolean hasRoot(double x){
+    public boolean hasRoot(double x) {
         return evaluate(x) == 0;
     }
 

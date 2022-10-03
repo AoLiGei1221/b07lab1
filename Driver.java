7a76d00a56290e613dc8b2d13479f54e55da2bb0
张浩芊 <zhanghaoqian@ZHQ-MBP-14-D.local>
2022-10-03 14:19:35 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Driver {
-    public static void main(String [] args) {
-        Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
-        Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
-        if(s.hasRoot(1))
-            System.out.println("1 is a root of s");
-        else
-            System.out.println("1 is not a root of s");
+    public static void main(String[] args) throws IOException {
+        double [] c1 = {-9, 6, 5};
+        int [] e1 = {2, 1, 4};
+        Polynomial p1 = new Polynomial(c1, e1);
+        double[] c2 = {-9, 8, 4, -5};
+        int[]    e2 = {2,  3, 4,  5};
+        Polynomial p2 = new Polynomial(c2, e2);
+        System.out.println("ADD");
+        Polynomial np = p1.add(p2);
+        for (int i = 0; i < np.coefficients.length; i++)
+            System.out.print(np.coefficients[i] + " ");
+        System.out.println();
+        for (int i = 0; i < np.exp.length; i++)
+            System.out.print(np.exp[i] + " ");
+        System.out.println();
+
+        System.out.println("MULTIPLY");
+        np = p1.multiply(p2);
+        for (int i = 0; i < np.coefficients.length; i++)
+            System.out.print(np.coefficients[i] + " ");
+        System.out.println();
+
+        for (int i = 0; i < np.exp.length; i++)
+            System.out.print(np.exp[i] + " ");
+
+
+        System.out.println();
+
+        double [] b1 = {-2,-6,5};
+        int [] b2 = {1,0,4};
+        Polynomial f = new Polynomial(b1,b2);
+        double [] a1 = {-3,4,3,-4};
+        int [] a2 = {2,3,1,4};
+        Polynomial p3 = new Polynomial(a1,a2);
+        Polynomial s = f.multiply(p2);
+        System.out.println("Test multiply:");
+        for(int i = 0; i < s.coefficients.length; i++) {
+            System.out.println(s.coefficients[i]);
+            System.out.println(s.exp[i]);
+        }
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,38 +1,262 @@
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+
+
 public class Polynomial{
     double[] coefficients;
+    int[] exp;
 
     public Polynomial() {
         this.coefficients = new double[0];
+        this.exp = new int[0];
     }
 
-    public Polynomial(double[] coefficients) {
+    public void Read(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        String line = scanner.nextLine().replace("-", "+-");
+        if(line.charAt(0) == '+') line = line.substring(1);
+        scanner.close();
+        String [] arrOfNewline = line.split("\\+");
+        double[] a = new double[arrOfNewline.length];
+        int[] b = new int[arrOfNewline.length];
+        int idx_a = 0, idx_b = 0;
+        for (String n : arrOfNewline){
+            boolean flag = (n.charAt(n.length() - 1) == 'x');
+            String[] sub = n.split("x");
+            //check if it is just constant.(check if len of array = 1)
+            if(sub.length == 1 && !flag){
+                a[idx_a++] = Double.parseDouble(sub[0]);
+                b[idx_b++] = 0;
+                continue;
+            }
+            //check if its exp is 1
+            if(sub.length == 1){
+                a[idx_a++] = Double.parseDouble(sub[0]);
+                b[idx_b++] = 1;
+                flag = false;
+                continue;
+            }
+            a[idx_a++] = Double.parseDouble(sub[0]);
+            b[idx_b++] = Integer.parseInt(sub[1]);
+        }
+        coefficients = a;
+        exp = b;
+    }
+
+    public Polynomial(double[] coefficients, int[] exp) {
         this.coefficients = coefficients;
+        this.exp = exp;
+    }
+
+    //--------------------------------------------
+    //helper method for add and multiply
+    public boolean check(int[] exp) {
+        boolean flag = false;
+        for (int i = 0; i < this.exp.length - 1; i++){
+            if (this.exp[i] > this.exp[i + 1]){
+                flag = true;
+                break;
+            }
+        }
+        return flag;
     }
 
     public Polynomial add(Polynomial other) {
-        double [] array;
+        //if they are empty
+        if (this.exp.length == 0)
+            return new Polynomial(other.coefficients, other.exp);
+        if (other.exp.length == 0)
+            return new Polynomial(this.coefficients, this.exp);
 
-        int len_this = this.coefficients.length;
-        int len_other = other.coefficients.length;
+        //check if it is in ascending order
+        boolean this_Flag = check(this.exp);
+        boolean other_Flag = check(other.exp);
+        //sort array by using selection sort
+        if(this_Flag) {
+            int n = this.exp.length;
+            for (int i = 0; i < n - 1; i++) {
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (this.exp[j] < this.exp[min_idx])
+                        min_idx = j;
 
-        array = new double[len_this > len_other ? len_this : len_other];
-
-        for(int i = 0; i < this.coefficients.length; i++) {
-            array[i] = array[i] + this.coefficients[i];
+                int temp1 = this.exp[min_idx];
+                this.exp[min_idx] = this.exp[i];
+                this.exp[i] = temp1;
+                double temp2 = this.coefficients[min_idx];
+                this.coefficients[min_idx] = this.coefficients[i];
+                this.coefficients[i] = temp2;
+            }
         }
+        if(other_Flag){
+            int k = other.exp.length;
+            for (int x = 0; x < k - 1; x++) {
+                int min_idx_other = x;
+                for (int y = x + 1; y < k; y++)
+                    if (other.exp[y] < other.exp[min_idx_other])
+                        min_idx_other = y;
 
-        for(int j = 0; j < other.coefficients.length; j++) {
-            array[j] = array[j] + other.coefficients[j];
+                int temp3 = other.exp[min_idx_other];
+                other.exp[min_idx_other] = other.exp[x];
+                other.exp[x] = temp3;
+                double temp4 = other.coefficients[min_idx_other];
+                other.coefficients[min_idx_other] = other.coefficients[x];
+                other.coefficients[x] = temp4;
+            }
         }
+        int max_exp = Math.max(this.exp[this.exp.length - 1], other.exp[other.exp.length -1]);
+        double[] temp_coe = new double[max_exp + 1];
+        for (int i = 0; i < this.coefficients.length; i++)
+            temp_coe[this.exp[i]] += this.coefficients[i];
 
-        return new Polynomial(array);
+        for (int i = 0; i < other.coefficients.length; i++)
+            temp_coe[other.exp[i]] += other.coefficients[i];
+
+        // count how many non-zero coefficients and accelerate length
+        int len = 0;
+        for (double v : temp_coe)
+            if (v != 0)
+                len++;
+
+        double[] new_coe = new double[len];
+        int[] new_exp = new int[len];
+
+        int j = 0;
+        for (int i = 0; i < temp_coe.length; i++) {
+            if (temp_coe[i] != 0) {
+                new_coe[j] = temp_coe[i];
+                new_exp[j] = i;
+                j = j + 1;
+            }
+        }
+        return new Polynomial(new_coe, new_exp);
+    }
+
+    public Polynomial multiply(Polynomial other) {
+        //if they are empty
+        if (this.exp.length == 0)
+            return new Polynomial(other.coefficients, other.exp);
+        if (other.exp.length == 0)
+            return new Polynomial(this.coefficients, this.exp);
+
+        //check if it is in ascending order
+        boolean this_Flag = check(this.exp);
+        boolean other_Flag = check(other.exp);
+        //sort array by using selection sort
+        if(this_Flag) {
+            int n = this.exp.length;
+            for (int i = 0; i < n - 1; i++) {
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (this.exp[j] < this.exp[min_idx])
+                        min_idx = j;
+
+                int temp1 = this.exp[min_idx];
+                this.exp[min_idx] = this.exp[i];
+                this.exp[i] = temp1;
+                double temp2 = this.coefficients[min_idx];
+                this.coefficients[min_idx] = this.coefficients[i];
+                this.coefficients[i] = temp2;
+            }
+        }
+        if(other_Flag){
+            int k = other.exp.length;
+            for (int x = 0; x < k - 1; x++) {
+                int min_idx_other = x;
+                for (int y = x + 1; y < k; y++)
+                    if (other.exp[y] < other.exp[min_idx_other])
+                        min_idx_other = y;
+
+                int temp3 = other.exp[min_idx_other];
+                other.exp[min_idx_other] = other.exp[x];
+                other.exp[x] = temp3;
+                double temp4 = other.coefficients[min_idx_other];
+                other.coefficients[min_idx_other] = other.coefficients[x];
+                other.coefficients[x] = temp4;
+            }
+        }
+        //find the largest exp in my array
+        int max_this = this.exp[0], max_other = other.exp[0];
+        for(int i = 1; i < this.exp.length; i++) {
+            if(this.exp[i] > max_this)
+                max_this = this.exp[i];
+        }
+        for(int j = 1; j < other.exp.length; j++) {
+            if(other.exp[j] > max_other)
+                max_other = other.exp[j];
+        }
+        double[] product = new double[max_this + max_other + 1];
+        for(int i = 0; i < this.exp.length; i++) {
+            for(int j = 0; j < other.exp.length; j++) {
+                int index = this.exp[i] + other.exp[j];
+                product[index] = product[index] + (this.coefficients[i] * other.coefficients[j]);
+            }
+        }
+        //count how many none-zero element
+        int count = 0;
+        for (double v : product) {
+            if (v == 0) {
+                ;
+            }
+            else count++;
+        }
+        double[] coe = new double[count];
+        int[] exp = new int[count];
+        for(int i = 0, index = 0; i < product.length; i++) {
+            if(product[i] != 0) {
+                coe[index] = product[i];
+                exp[index] = i;
+                index++;
+            }
+        }
+        return new Polynomial(coe,exp);
+    }
+    //--------------------------------------------
+    public void saveToFile(String filename) throws IOException {
+        FileWriter myWriter = new FileWriter(filename);
+        StringBuilder temp = new StringBuilder();
+        for(int i = 0; i < this.coefficients.length; i++){
+            //if it is constant and first number
+            if(exp[i] == 0 && i == 0){temp.append(coefficients[i]); continue;}
+            //if it is constant and not the first number
+            if(exp[i] == 0){
+                if(coefficients[i] < 0) temp.append(coefficients[i]);
+                else temp.append("+").append(coefficients[i]);
+                continue;
+            }
+            //if it is negative and positive number and first number and its exp = 1
+            if(coefficients[i] < 0 && i == 0 && exp[i] == 1){temp.append(coefficients[i]).append("x"); continue;}
+            if(coefficients[i] > 0 && i == 0 && exp[i] == 1){temp.append(coefficients[i]).append("x"); continue;}
+            //if it is negative and positive and first number but exp not 1
+            if(coefficients[i] < 0 && i == 0){temp.append(coefficients[i]).append("x").append(exp[i]); continue;}
+            if(coefficients[i] > 0 && i == 0){temp.append(coefficients[i]).append("x").append(exp[i]); continue;}
+            //if it is not the first number but exp = 1
+            if(coefficients[i] < 0 && exp[i] == 1){temp.append(coefficients[i]).append("x");continue;}
+            if(coefficients[i] > 0 && exp[i] == 1){
+                temp.append("+").append(coefficients[i]).append("x");
+                continue;
+            }
+            if(coefficients[i] < 0) temp.append(coefficients[i]).append("x").append(exp[i]);
+            else temp.append("+").append(coefficients[i]).append("x").append(exp[i]);
+        }
+        myWriter.write(temp.toString());
+        myWriter.close();
     }
 
     public double evaluate(double x) {
         double sum = 0.0;
         for(int i = 0; i < coefficients.length; i++) {
-            double j = coefficients[i];
-            sum = sum + j * Math.pow(x, i);
+            //get the coefficient
+            double coe = coefficients[i];
+            //get the exp
+            int exponential = exp[i];
+            //check if it is just a constant
+            if(exp[i] == 0){sum += coe; continue;}
+            sum = sum + coe * Math.pow(x, exponential);
         }
         return sum;
     }
@@ -40,9 +264,4 @@ public class Polynomial{
     public boolean hasRoot(double x) {
         return evaluate(x) == 0;
     }
-
-
-
-
-
 }
